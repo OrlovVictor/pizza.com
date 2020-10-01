ef7ce9f282f3b2e2ccf3603cf26b67f36406473d
Victor Orlov <orlov.viktor@gmail.com>
2020-10-01 20:03:13 +0300

    new package installed: lavary/laravel-menu.

--- config/app.php
+++ config/app.php
@@ -227,6 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+	    'Menu'      => Lavary\Menu\Facade::class,
+
     ],
 
 ];
