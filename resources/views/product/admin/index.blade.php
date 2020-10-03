a44da401b4f7281c6c5fced3b24938a4d8742dbe
Victor Orlov <orlov.viktor@gmail.com>
2020-10-03 21:30:58 +0300

    admin page: fixed messages in the confirmation dialog.

--- resources/views/product/admin/index.blade.php
+++ resources/views/product/admin/index.blade.php
@@ -64,14 +64,14 @@
 		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title">Please confirm deleting</h5>
+					<h5 class="modal-title">Delete confirmation</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
 				<div class="modal-body">
 					<div class="js_item_name"></div>
-					Are you sure to delete this item?
+					Are you sure you want to delete this item?
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
